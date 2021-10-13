b088a02d383ef885fb748f9245db64355f8668f8
eshaant7 <68991885+eshaant7@users.noreply.github.com>
2021-10-13 09:09:39 +0100

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -1,44 +1,42 @@
-
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
+import csv
 import requests
 import pandas as pd
 
-
-url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
-
-page = requests.get(url)
+start_url = "https://en.wikipedia.org/wiki/List_of_brown_dwarfs"
+page = requests.get(start_url)
 print(page)
+def scrape():
+    headers = ["Star", "Distance", "Mass", "Radius"]
+    star_data = []
 
-soup = bs(page.text,'html.parser')
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-star_table = soup.find_all('table')
-print(len(star_table))
-
-
-temp_list= []
-table_rows = star_table[4].find_all('tr')
-for tr in table_rows:
-    td = tr.find_all('td')
-    row = [i.text.rstrip() for i in td]
-    temp_list.append(row)
-print(temp_list)
-
-
-
-Star_names = []
-Distance =[]
-Mass = []
-Radius =[]
-
-
-for i in range(1,len(temp_list)):
+    star_table = soup.find('table')
+    temp_list = []
+    table_rows = star_table.find_all('tr')
     
-    Star_names.append(temp_list[i][0])
-    Distance.append(temp_list[i][5])
-    Mass.append(temp_list[i][8])
-    Radius.append(temp_list[i][9])
+    for tr in table_rows:
+        td = tr.find_all('td')
+        row = [i.text.rstrip() for i in td] 
+        temp_list.append(row)
+        
+    Star = []
+    Distance =[]
+    Mass = []
+    Radius =[]
 
-df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
-print(df2)
+    for i in range(1,len(temp_list)):
+        Star.append(temp_list[i][0]) 
+        Distance.append(temp_list[i][5])
+        Mass.append(temp_list[i][7]) 
+        Radius.append(temp_list[i][8]) 
+        
+        
+    df2 = pd.DataFrame(list(zip(Star,Distance,Mass,Radius)),columns=['Star','Distance','Mass','Radius'])
+    
+    print(df2)
 
-df2.to_csv('dwarf_stars.csv')
+    df2.to_csv('brown_dwarfs.csv')
+
+scrape()
